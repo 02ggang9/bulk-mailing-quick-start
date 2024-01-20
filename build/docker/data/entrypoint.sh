474967cd88ed693051221ad11aba13051b12cb9b
02ggang9 <02ggang9@gmail.com>
2024-01-20 18:08:40 +0900

    fix: 경로 수정

--- build/docker/data/entrypoint.sh
+++ build/docker/data/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cp /application.yml /home/doo-re/src/main/resources/application.yml
+cp /application.yml /home/mail/src/main/resources/application.yml
 
-cd /home/doo-re
+cd /home/mail
 
 gradle build --daemon --build-cache --parallel
 
-java -jar -Duser.timezone=Asia/Seoul /home/doo-re/build/libs/spring-batch-0.0.1-SNAPSHOT.jar
+java -jar -Duser.timezone=Asia/Seoul /home/mail/build/libs/spring-batch-0.0.1-SNAPSHOT.jar
